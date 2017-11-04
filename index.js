0c3b529df6a63bd9ad188c1462089a42ab0705a4
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-04 11:55:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function updateObjectWithKeyAndValue(object, key, value){
   });
   return newObj;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  var newObj = Object.assign(object,{
+    [key]:value
+  });
+  return newObj;
+}
